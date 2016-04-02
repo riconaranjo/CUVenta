2f968c3af72c87e6175af465eca0ca810c03fcd1
Arun Galva <arungalva@gmail.com>
2016-04-02 12:26:54 -0400

    initial setup with hello world page

---
+++ application/views/index.php
@@ -0,0 +1,6 @@
+<?php include ('templates/default/header.php')  ?>
+<body>
+	<?php include('templates/default/navbar.php') ?>
+	<?php include('templates/default/scripts.php') ?>
+</body>
+<?php include('templates/default/footer.php') ?>
---
+++ application/views/templates/default/scripts.php
@@ -0,0 +1,4 @@
+<script src="<?php echo base_url("assets/js/jquery-1.12.2.min"); ?>"></script>
+ <script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+ <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
